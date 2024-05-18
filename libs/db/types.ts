34887707573f783ed45f3b62524da38d34628c88
Indu Chandana <indu.99.ic@gmail.com>
2024-05-18 19:37:48 +0530

    May18 001 lib/db with local postgres use docker

---
+++ libs/db/types.ts
@@ -0,0 +1,4 @@
+// They can install the "@foundation-trpc/db" package (name of the package.json)library and they can import any type of types
+//from the Prisma client in the types route.
+
+export * from '@prisma/client'
