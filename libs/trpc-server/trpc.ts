301380a6db9da1427e1617a96aa8979938cc527d
Indu Chandana <indu.99.ic@gmail.com>
2024-06-01 12:45:07 +0530

    June01 002 trpc-server-authorization done.

--- libs/trpc-server/trpc.ts
+++ libs/trpc-server/trpc.ts
@@ -6,4 +6,4 @@ export const t = initTRPC.context<typeof createTRPCContext>().create()
 
 export const router = t.router
 export const publicProcedure = t.procedure
-export const privateProcedure = t.procedure.use(isAuthed())
+export const privateProcedure = t.procedure.use(isAuthed('admin', 'manager'))
